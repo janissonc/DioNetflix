6fd713fec0d4943a9c8d6555f0bd584821183a0a
janisson <janisson.bass@gmail.com>
2022-04-21 21:04:38 -0300

    -a

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:false,
+    dots:false,
     responsive:{
         0:{
             items:1
@@ -14,3 +15,13 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+
+
+function assistirAgora(){
+    window.open("https://www.youtube.com/watch?v=i2vEAaEyHco&ab_channel=ImagemFilmes", '_blank'); 
+}
+
+function maisInformacoes(){
+    window.open("https://www.themoviedb.org/movie/76163-the-expendables-2?language=pt-BR", '_blank'); 
+}
